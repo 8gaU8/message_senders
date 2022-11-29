f15cfce402b66f3317c8edf11d3a3e42852721ab
straxFromIbr <haga.yuya2.2016@gmail.com>
2022-11-29 14:47:32 +0900

    Slackへの投稿クラス追加

--- message_senders/__init__.py
+++ message_senders/__init__.py
@@ -3,6 +3,7 @@ from .discord_sender import DiscordSender
 from .file_sender import FileSender
 from .line_sender import LineSender
 from .remote_file_sender import RemoteFileSender
+from .slack_sender import SlackSender
 from .stdout_sender import StdOutSender
 from .tweet_sender import TweetSender
 
@@ -12,6 +13,7 @@ __all__ = [
     "FileSender",
     "LineSender",
     "RemoteFileSender",
+    "SlackSender",
     "StdOutSender",
     "TweetSender",
 ]
