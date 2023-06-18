7f1a891f6324a6ed5a6ac0ac0c259730456e0002
straxFromIbr <haga.yuya2.2016@gmail.com>
2023-06-18 23:03:30 +0900

    version info

--- build/lib/message_senders/stdout_sender.py
+++
@@ -1,9 +0,0 @@
-from .base_sender import BaseSender
-
-
-class StdOutSender(BaseSender):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def send(self, text: str):
-        print(text)
